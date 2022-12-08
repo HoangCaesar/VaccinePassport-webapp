d53cf67dbbf0e8a74d7ed265ec89732e894c11c1
Caesar <vuhoang7398@gmail.com>
2022-12-08 14:23:16 +0200

    Adjust API

--- src/api/configApi.js
+++ src/api/configApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:3000/api';
+export const BASE_URL = 'https://vaacinepassport-api.onrender.com/api';
 export const USER_ENDPOINT = '/user';
 export const VACCINE_ENDPOINT = '/vaccine';
 export const VACCINE_LOTS_ENDPOINT = '/vaccine/lots';
